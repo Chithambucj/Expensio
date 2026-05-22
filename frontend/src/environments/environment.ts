35e367f1fd1f3e4d5ba29481378e5ec5a4924708
Chithambucj <chithambucj@gmail.com>
2026-05-23 00:58:09 +0530

    7th Fixed CORS and OPTIONS preflight issues

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Use local backend during development. Adjust if needed.
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8084'
 };
