41cc8d47402f7f7304c11fd4b49650b7a699a965
Chithambucj <chithambucj@gmail.com>
2026-05-23 09:52:14 +0530

    9th Fixed CORS and OPTIONS preflight issues

---
+++ backend/src/main/java/com/expense/tracker/config/GlobalCorsFilter.java
@@ -0,0 +1,54 @@
+package com.expense.tracker.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GlobalCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+
+        HttpServletResponse res =
+                (HttpServletResponse) response;
+
+        HttpServletRequest req =
+                (HttpServletRequest) request;
+
+        res.setHeader(
+                "Access-Control-Allow-Origin",
+                "https://expensio-tracking.netlify.app"
+        );
+
+        res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS"
+        );
+
+        res.setHeader(
+                "Access-Control-Allow-Headers",
+                "*"
+        );
+
+        res.setHeader(
+                "Access-Control-Allow-Credentials",
+                "true"
+        );
+
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
